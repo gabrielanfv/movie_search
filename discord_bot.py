a0d0728add6181bdd4400d405cc35c60ed23fa4a
Gabriela <gabriela.flores1@upr.edu>
2020-07-16 22:48:46 -0400

    Integrated into a Discord bot

---
+++ discord_bot.py
@@ -0,0 +1,24 @@
+import imdb_random_movies
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$movie'):
+        
+        await message.channel.send(imdb_random_movies.get_movie())
+
+client.run(os.getenv('DISCORD_TOKEN'))
--- imdb_random_movies.py
+++ imdb_random_movies.py
@@ -1,23 +1,24 @@
+
 import requests
 from bs4 import BeautifulSoup
 import random
 
-#select a random genre 
-genre = random.choice(['action', 'action,comedy', 'adventure', 'animation', 'comedy', 'comedy,romance', 'crime', 'drama', 'fantasy', 'horror', 'mystery', 'romance', 'sci-fi', 'thriller'])
-print(genre)
+def get_movie():
+    #select a random genre 
+    genre = random.choice(['action', 'action,comedy', 'adventure', 'animation', 'comedy', 'comedy,romance', 'crime', 'drama', 'fantasy', 'horror', 'mystery', 'romance', 'sci-fi', 'thriller'])
 
-URL = "https://www.imdb.com/search/title/?genres="+ genre +"&explore=title_type,genres&title_type=movie&ref_=adv_explore_rhs"
-r = requests.get(URL)
+    URL = "https://www.imdb.com/search/title/?genres="+ genre +"&explore=title_type,genres&title_type=movie&ref_=adv_explore_rhs"
+    r = requests.get(URL)
 
-soup = BeautifulSoup(r.content, 'html5lib')
-table = soup.find('div', attrs = {'class':'lister-list'})
+    soup = BeautifulSoup(r.content, 'html5lib')
+    table = soup.find('div', attrs = {'class':'lister-list'})
 
-films = []
+    films = []
 
-#look for all the names of the movies that appear on that page
-for row in table.findAll('h3', attrs = {'class':'lister-item-header'}):
-    films.append(row.find('a').text)
+    #look for all the names of the movies that appear on that page
+    for row in table.findAll('h3', attrs = {'class':'lister-item-header'}):
+        films.append(row.find('a').text)
 
 
-#select a random movie
-print(random.choice(films))
+    #select a random movie
+    return  "Genre: " + genre + " | Movie: " + random.choice(films)
